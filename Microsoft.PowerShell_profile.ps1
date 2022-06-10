4a7d75c2a82fe5200eae124c3455a22d712a224d
Willem Boshoff <willem.boshoff@dyna-mo.com>
2022-06-10 11:35:35 +0200

    Update config for powershell core

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Starship
+Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "C:\Users\willem.boshoff\code\terminology\starship.toml"
+
+# Oh my Posh
+#oh-my-posh --init --shell pwsh --config ~/code/terminology/oh-my-posh-settings.json | Invoke-Expression
+
+# Better icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+Set-Alias -Name grep -Value rg
