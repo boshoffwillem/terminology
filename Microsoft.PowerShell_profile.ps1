3f039d078e87096c98e5574bb81605a39ab48252
Willem Boshoff <willem.boshoff@dyna-mo.com>
2022-10-24 10:25:30 +0200

    Update configs

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,7 +9,7 @@ Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "C:\Users\willem.boshoff\code\terminology\starship.toml"
 
 # Oh my Posh
-#oh-my-posh --init --shell pwsh --config ~/code/terminology/oh-my-posh-settings.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config ~/code/terminology/oh-my-posh-settings.json | Invoke-Expression
 
 # Better icons
 Import-Module -Name Terminal-Icons
@@ -21,3 +21,5 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
 Set-Alias -Name grep -Value rg
+Set-Alias -Name v -Value nvim
+Set-Alias -Name e -Value emacs -nw
